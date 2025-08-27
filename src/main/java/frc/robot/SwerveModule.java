316f534007f821bbb921d63b78013563b932c3e8
Evan Wang <evwang07@gmail.com>
2025-08-27 15:25:54 -0700

    Add documentation for the SwerveModule class

--- src/main/java/frc/robot/SwerveModule.java
+++ src/main/java/frc/robot/SwerveModule.java
@@ -14,22 +14,29 @@ import frc.lib.math.Conversions;
 import frc.lib.util.SwerveModuleConstants;
 
 public class SwerveModule {
-    public int moduleNumber;
-    private Rotation2d angleOffset;
+    public int moduleNumber; // The ID number of the swerve module (FL = 0, FR = 1, BL = 2, BR = 3)
+    private Rotation2d angleOffset; // Measured for each absolute encoder to set the swerve modules' "zero" rotation (CANCoder)
 
-    private TalonFX mAngleMotor;
-    private TalonFX mDriveMotor;
-    private CANcoder angleEncoder;
+    private TalonFX mAngleMotor; // Rotates the wheel of the swerve module like a compass ("azimuth" motor)
+    private TalonFX mDriveMotor; // Spins the wheel of the swerve module (makes the robot chassis actually translationally move!)
+    private CANcoder angleEncoder; // The absolute encoder that constantly measures swerve module rotation
 
     private final SimpleMotorFeedforward driveFeedForward = new SimpleMotorFeedforward(Constants.Swerve.driveKS, Constants.Swerve.driveKV, Constants.Swerve.driveKA);
 
-    /* drive motor control requests */
+    /* Drive motor control (output) requests */
     private final DutyCycleOut driveDutyCycle = new DutyCycleOut(0);
     private final VelocityVoltage driveVelocity = new VelocityVoltage(0);
 
-    /* angle motor control requests */
+    /* Angle (azimuth) motor control (output) requests */
     private final PositionVoltage anglePosition = new PositionVoltage(0);
 
+    /**
+     * Creates a SwerveModule object that represents a physical swerve module
+     * @param moduleNumber The ID number of the swerve module (FL = 0, FR = 1, BL = 2, BR = 3)
+     * @param moduleConstants A SwerveModuleConstants object that contains the swerve module motor IDs, absolute encoder ID, and 
+     * absolute encoder angle offset
+     * @return A new SwerveModule object
+     */
     public SwerveModule(int moduleNumber, SwerveModuleConstants moduleConstants){
         this.moduleNumber = moduleNumber;
         this.angleOffset = moduleConstants.angleOffset;
@@ -49,12 +56,26 @@ public class SwerveModule {
         mDriveMotor.getConfigurator().setPosition(0.0);
     }
 
+    /**
+     * Sets the speeds (voltages) of the angle (azimuth) and drive motors of the swerve module
+     * @param desiredState A SwerveModuleState object representing the desired angular position of the angle (azimuth) motor and 
+     * desired speed of the drive motor for the swerve module
+     * @param isOpenLoop If open loop mode should be used. Use open loop mode for teleoperated swerve drive and closed loop mode (having 
+     * isOpenLoop = false) for autonomous swerve drive
+     */
     public void setDesiredState(SwerveModuleState desiredState, boolean isOpenLoop){
         desiredState.optimize(getState().angle); 
         mAngleMotor.setControl(anglePosition.withPosition(desiredState.angle.getRotations()));
         setSpeed(desiredState, isOpenLoop);
     }
 
+    /**
+     * Sets the speed (voltage) of the drive motor of the swerve module
+     * @param desiredState A SwerveModuleState object representing the desired angular position of the angle (azimuth) motor and 
+     * desired speed of the drive motor for the swerve module
+     * @param isOpenLoop If open loop mode should be used. Use open loop mode for teleoperated swerve drive and closed loop mode (having 
+     * isOpenLoop = false) for autonomous swerve drive
+     */
     private void setSpeed(SwerveModuleState desiredState, boolean isOpenLoop){
         if(isOpenLoop){
             driveDutyCycle.Output = desiredState.speedMetersPerSecond / Constants.Swerve.maxSpeed;
@@ -67,15 +88,29 @@ public class SwerveModule {
         }
     }
 
+    /**
+     * Gets the absolute angular position of the swerve module using the CANCoder
+     * @return A Rotation2d object representing the absolute angular position of the swerve module
+     */
     public Rotation2d getCANcoder(){
         return Rotation2d.fromRotations(angleEncoder.getAbsolutePosition().getValueAsDouble());
     }
 
+    /**
+     * Corrects the absolute angular position measured by the CANCoder by subtracting the CANCoder's angular offset from the former. 
+     * Assuming the CANCoder's offsets have been measured correctly, this method ensures the swerve modules' "zero" angular 
+     * positions are the same
+     */
     public void resetToAbsolute(){
         double absolutePosition = getCANcoder().getRotations() - angleOffset.getRotations();
         mAngleMotor.setPosition(absolutePosition);
     }
 
+    /**
+     * Gets the current angular position of the angle (azimuth) motor and current speed of the drive motor for the swerve module
+     * @return A SwerveModuleState object containing the current angular position of the angle (azimuth) motor and current speed 
+     * of the drive motor for the swerve module 
+     */
     public SwerveModuleState getState(){
         return new SwerveModuleState(
             Conversions.RPSToMPS(mDriveMotor.getVelocity().getValueAsDouble(), Constants.Swerve.wheelCircumference), 
@@ -83,6 +118,11 @@ public class SwerveModule {
         );
     }
 
+    /**
+     * Gets the current positions of the angle (azimuth) motor and the drive motor for the swerve module
+     * @return A SwerveModulePosition object containing the current positions of the angle (azimuth) motor 
+     * and the drive motor for the swerve module
+     */
     public SwerveModulePosition getPosition(){
         return new SwerveModulePosition(
             Conversions.rotationsToMeters(mDriveMotor.getPosition().getValueAsDouble(), Constants.Swerve.wheelCircumference), 
